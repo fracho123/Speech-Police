567c6ce73f86b732855faecfe032e7dd17e9acac
Glenn Sun <sun.glenn@outlook.com>
2020-01-25 17:28:17 -0800

    Join/leave voice channel

--- index.js
+++ index.js
@@ -21,17 +21,24 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
+        function findVoiceChannel() {
+            for (const id in bot.channels) {
+                if (bot.channels[id].type == 2) {
+                    return id;
+                }
+            }
+        }
+
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
             // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
+            case 'join':
+                bot.joinVoiceChannel(findVoiceChannel());
+            case 'leave':
+                bot.leaveVoiceChannel(findVoiceChannel());
             break;
             // Just add any case commands if you want to..
          }
